5364532dd14d1aed63af9bfc7ea6a6a4cbcf6962
Evan Tucker <eej.tucker@gmail.com>
2017-08-13 20:56:18 -0600

    added sounds and new charcters

--- app/data/characters.js
+++ app/data/characters.js
@@ -18,28 +18,129 @@ var characters = [
     /* question 12: */ "1"
             ],
     description: "From Bastard of Winterfell, to King in the North, Jon Snow is a proven warrior and leader, He is loyal, fair, and honorable to a fault.  Jon Snow will make a valuable ally in the wars to come.",
-    sound: "assets/sounds/mainTheme.mp3"
+    sound: "assets/sounds/jonSnow-winter.mp3"
   },
   {
-    name: "Tyrion Lannister",
-    photo: "https://upload.wikimedia.org/wikipedia/en/5/50/Tyrion_Lannister-Peter_Dinklage.jpg",
-    scores: ["5", "5", "5", "5", "5", "5", "5", "5", "5", "5", "5", "5"]
+    name: "Brandon Stark",
+    photo: "assets/images/jonSnow.jpg",
+    scores: [
+    /* question 1: */  "2",
+    /* question 2: */  "2",
+    /* question 3: */  "2",
+    /* question 4: */  "2",
+    /* question 5: */  "2",
+    /* question 6: */  "2",
+    /* question 7: */  "2",
+    /* question 8: */  "2", 
+    /* question 9: */  "2",
+    /* question 10: */ "2",
+    /* question 11: */ "2",
+    /* question 12: */ "2"
+            ],
+    description: "From Bastard of Winterfell, to King in the North, Jon Snow is a proven warrior and leader, He is loyal, fair, and honorable to a fault.  Jon Snow will make a valuable ally in the wars to come.",
+    sound: "assets/sounds/branStark.mp3"
   },
   {
-    name: "HODOR",
-    photo: "https://upload.wikimedia.org/wikipedia/en/5/50/Tyrion_Lannister-Peter_Dinklage.jpg",
-    scores: ["3", "3", "3", "3", "3", "3", "3", "3", "3", "3", "3", "3"]
-  },
-  {
-    name: "Samwell Tarly",
-    photo: "https://upload.wikimedia.org/wikipedia/en/5/50/Tyrion_Lannister-Peter_Dinklage.jpg",
-    scores: ["2", "2", "2", "2", "2", "2", "2", "2", "2", "2", "2", "2"]
+    name: "Rob Stark",
+    photo: "assets/images/jonSnow.jpg",
+    scores: [
+    /* question 1: */  "3",
+    /* question 2: */  "3",
+    /* question 3: */  "3",
+    /* question 4: */  "3",
+    /* question 5: */  "3",
+    /* question 6: */  "3",
+    /* question 7: */  "3",
+    /* question 8: */  "3", 
+    /* question 9: */  "3",
+    /* question 10: */ "3",
+    /* question 11: */ "3",
+    /* question 12: */ "3"
+            ],
+    description: "From Bastard of Winterfell, to King in the North, Jon Snow is a proven warrior and leader, He is loyal, fair, and honorable to a fault.  Jon Snow will make a valuable ally in the wars to come.",
+    sound: "assets/sounds/robStark.mp3"
   },
   {
     name: "Arya Stark",
-    photo: "https://upload.wikimedia.org/wikipedia/en/5/50/Tyrion_Lannister-Peter_Dinklage.jpg",
-    scores: ["4", "4", "4", "4", "4", "4", "4", "4", "4", "4", "4", "4"]
-  }
+    photo: "assets/images/jonSnow.jpg",
+    scores: [
+    /* question 1: */  "4",
+    /* question 2: */  "4",
+    /* question 3: */  "4",
+    /* question 4: */  "4",
+    /* question 5: */  "4",
+    /* question 6: */  "4",
+    /* question 7: */  "4",
+    /* question 8: */  "4", 
+    /* question 9: */  "4",
+    /* question 10: */ "4",
+    /* question 11: */ "4",
+    /* question 12: */ "4"
+            ],
+    description: "From Bastard of Winterfell, to King in the North, Jon Snow is a proven warrior and leader, He is loyal, fair, and honorable to a fault.  Jon Snow will make a valuable ally in the wars to come.",
+    sound: "assets/sounds/aryaStark.mp3"
+  },
+  {
+    name: "Sansa Stark",
+    photo: "assets/images/jonSnow.jpg",
+    scores: [
+    /* question 1: */  "5",
+    /* question 2: */  "5",
+    /* question 3: */  "5",
+    /* question 4: */  "5",
+    /* question 5: */  "5",
+    /* question 6: */  "5",
+    /* question 7: */  "5",
+    /* question 8: */  "5", 
+    /* question 9: */  "5",
+    /* question 10: */ "5",
+    /* question 11: */ "5",
+    /* question 12: */ "5"
+            ],
+    description: "From Bastard of Winterfell, to King in the North, Jon Snow is a proven warrior and leader, He is loyal, fair, and honorable to a fault.  Jon Snow will make a valuable ally in the wars to come.",
+    sound: "assets/sounds/sansaStark.mp3"
+  },
+  {
+    name: "Cateyln Stark",
+    photo: "assets/images/jonSnow.jpg",
+    scores: [
+    /* question 1: */  "1",
+    /* question 2: */  "2",
+    /* question 3: */  "3",
+    /* question 4: */  "4",
+    /* question 5: */  "5",
+    /* question 6: */  "1",
+    /* question 7: */  "2",
+    /* question 8: */  "3", 
+    /* question 9: */  "4",
+    /* question 10: */ "5",
+    /* question 11: */ "1",
+    /* question 12: */ "2"
+            ],
+    description: "From Bastard of Winterfell, to King in the North, Jon Snow is a proven warrior and leader, He is loyal, fair, and honorable to a fault.  Jon Snow will make a valuable ally in the wars to come.",
+    sound: "assets/sounds/catelynStark.mp3"
+  },
+  {
+    name: "Ned Stark",
+    photo: "assets/images/jonSnow.jpg",
+    scores: [
+    /* question 1: */  "3",
+    /* question 2: */  "3",
+    /* question 3: */  "3",
+    /* question 4: */  "3",
+    /* question 5: */  "4",
+    /* question 6: */  "4",
+    /* question 7: */  "4",
+    /* question 8: */  "5", 
+    /* question 9: */  "5",
+    /* question 10: */ "1",
+    /* question 11: */ "1",
+    /* question 12: */ "1"
+            ],
+    description: "From Bastard of Winterfell, to King in the North, Jon Snow is a proven warrior and leader, He is loyal, fair, and honorable to a fault.  Jon Snow will make a valuable ally in the wars to come.",
+    sound: "assets/sounds/nedStark.mp3"
+  },
+  
 ];
 
 module.exports = characters;
